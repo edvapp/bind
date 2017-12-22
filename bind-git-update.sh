c41610836db1fa299e2a152fe1a51f39be9b4286
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-12-22 13:05:40 +0100

    added new git-update-script

--- bind-git-update.sh
+++ bind-git-update.sh
@@ -2,23 +2,28 @@
 
 cd /etc/bind
 echo "################################################"
-echo "git checkout db.app.tsn db.10 ##################"
+echo "## git checkout db.app.tsn db.10               #"
+echo "################################################"
 sudo git checkout db.app.tsn
 sudo git checkout db.10
 
 echo "################################################"
-echo "git pull start #################################"
+echo "## git pull origin mastergit pull start        #"
+echo "################################################"
 sudo git pull origin master
 
 echo "################################################"
-echo "restart bind9 ##################################"
+echo "## systemctl restart bind9.service             #"
+echo "################################################"
 sudo systemctl restart bind9.service
 cd
 
 echo "################################################"
-echo "bind9 status ###################################"
+echo "## systemctl status bind9.service              #"
+echo "################################################"
 sudo systemctl status bind9.service
 
 echo "################################################"
-echo "bind9 journalctl ###############################"
+echo "## journalctl -u bind9.service                 #"
+echo "################################################"
 sudo journalctl -u bind9.service
